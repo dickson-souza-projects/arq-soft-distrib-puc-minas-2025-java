7dbd667181a58a5291f9526c364bf43da19b550b
Allyson Rodrigues <allyson.b.rodrigues@gmail.com>
2025-06-22 15:38:33 -0300

    Configuração inicial do projeto SpringBoot usando spring initializr

---
+++ src/main/java/br/com/arquitetura_plataforma_java/beacons/BeaconsApplication.java
@@ -0,0 +1,13 @@
+package br.com.arquitetura_plataforma_java.beacons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeaconsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeaconsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arquitetura_plataforma_java/beacons/BeaconsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arquitetura_plataforma_java.beacons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeaconsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
