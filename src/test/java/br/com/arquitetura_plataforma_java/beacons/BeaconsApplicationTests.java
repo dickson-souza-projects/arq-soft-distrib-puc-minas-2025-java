534d0a92a331f57e4f19bd1e0d8afb447d4f7d49
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-06-22 17:24:30 -0300

    Comentando testes (do template inicial) para teste da ação do SonarCloud

--- src/test/java/br/com/arquitetura_plataforma_java/beacons/BeaconsApplicationTests.java
+++ src/test/java/br/com/arquitetura_plataforma_java/beacons/BeaconsApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.arquitetura_plataforma_java.beacons;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BeaconsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package br.com.arquitetura_plataforma_java.beacons;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BeaconsApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
