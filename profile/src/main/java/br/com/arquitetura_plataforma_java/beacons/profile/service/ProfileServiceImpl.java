d25824cab6e7e2b2d35c9b2ac5e46ee3023440f5
allyson <allyson.b.rodrigues@gmail.com>
2025-06-30 23:57:53 -0300

    criação dos outros métodos

--- profile/src/main/java/br/com/arquitetura_plataforma_java/beacons/profile/service/ProfileServiceImpl.java
+++ profile/src/main/java/br/com/arquitetura_plataforma_java/beacons/profile/service/ProfileServiceImpl.java
@@ -1,6 +1,8 @@
 package br.com.arquitetura_plataforma_java.beacons.profile.service;
 
+import br.com.arquitetura_plataforma_java.beacons.profile.DTO.ProfileDTO;
 import br.com.arquitetura_plataforma_java.beacons.profile.entity.ProfileEntity;
+import br.com.arquitetura_plataforma_java.beacons.profile.mapper.ProfileMapper;
 import br.com.arquitetura_plataforma_java.beacons.profile.repository.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ProfileServiceImpl implements ProfileService {
@@ -20,13 +23,28 @@ public class ProfileServiceImpl implements ProfileService {
     @Autowired
     private ProfileRepository profileRepository;
 
+    @Autowired
+    private ProfileMapper profileMapper;
+
     @Override
-    public List<ProfileEntity> getAllUser() {
-        return profileRepository.findAll();
+    public List<ProfileDTO> getAllUser() {
+        return profileRepository.findAll().stream().map(profileMapper::toDTO).collect(Collectors.toList());
     }
 
     @Override
-    public void save(ProfileEntity profileEntity) {
-        profileRepository.save(profileEntity);
+    public ProfileDTO getUserById(int id) {
+        return profileMapper.toDTO(profileRepository.findById(id));
     }
+
+    @Override
+    public void save(ProfileDTO profileDTO) {
+        profileRepository.save(profileMapper.toEntity(profileDTO));
+    }
+
+    @Override
+    public void delete(int id) {
+        profileRepository.deleteById((long) id);
+    }
+
+
 }
